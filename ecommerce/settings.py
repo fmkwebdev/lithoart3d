61eb92b46ea6d415cdd6f38fbd696bfe99ac81bf
fmkwebdev <114531430+fmkwebdev@users.noreply.github.com>
2024-01-06 17:33:01 +0100

    =D

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
+    'storages',
 ]
 
 
@@ -144,3 +145,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 CSRF_COOKIE_DOMAIN = '.railway.app'
+
+
+AWS_ACCESS_KEY_ID = 'AKIAW3MEDHFY6JOQ3RPZ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'lithoart3d'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
