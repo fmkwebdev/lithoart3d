3006b20cc8dd095f6c2f6c2a1374fec717ede783
fmkwebdev <114531430+fmkwebdev@users.noreply.github.com>
2024-01-19 02:00:05 +0100

    1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
+    'csp',
     'storages',
 ]
 
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'csp.middleware.CSPMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.locale.LocaleMiddleware',
@@ -126,6 +128,8 @@ USE_TZ = True
 
 
 #sec
+
+CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
 
